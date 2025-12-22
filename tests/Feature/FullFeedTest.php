fd6ad97575cdda2bfab8fb98f368aabe62b727f4
puklipo <88759954+puklipo@users.noreply.github.com>
2025-12-22 13:48:10 +0900

    Add test for fetching full feed content
    Introduces a test to verify that FullFeed can fetch and return the full content of an article from a given URL using a mocked HTTP response.

--- tests/Feature/FullFeedTest.php
+++ tests/Feature/FullFeedTest.php
@@ -4,3 +4,14 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Http;
 use Revolution\Fullfeed\Facades\FullFeed;
+
+it('can fetch full feed content', function () {
+    Http::fake([
+        'https://laravel.com/blog/test' => Http::response('<html><body><article><h1>Full Article</h1><p>This is the full content of the article.</p></article></body></html>', 200),
+    ]);
+
+    $content = FullFeed::get('https://laravel.com/blog/test');
+
+    expect($content)->toContain('Full Article')
+        ->and($content)->toContain('This is the full content of the article.');
+});
