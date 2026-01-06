c8a0c64a8475278a0927f8b53f72067893549f55
kawax <kawaxbiz@gmail.com>
2026-01-06 14:59:08 +0900

    Sort and update plus.json entries, add sort command
    Reordered entries in resources/fullfeed/plus.json alphabetically by name and updated some URL patterns for consistency. Added a new Artisan command 'fullfeed:sort' in workbench/routes/console.php to automate sorting of plus.json by name for easier maintenance.

--- workbench/routes/console.php
+++ workbench/routes/console.php
@@ -4,12 +4,27 @@ declare(strict_types=1);
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
 use Revolution\Fullfeed\Facades\FullFeed;
 
 // Artisan::command('inspire', function () {
 //     $this->comment(Inspiring::quote());
 // })->purpose('Display an inspiring quote');
 
+/**
+ * Maintenance commands
+ */
+// vendor/bin/testbench fullfeed:sort
+Artisan::command('fullfeed:sort', function () {
+    $json = File::json(__DIR__.'/../../resources/fullfeed/plus.json');
+    usort($json, fn ($a, $b) => strcmp($a['name'], $b['name']));
+    File::put(__DIR__.'/../../resources/fullfeed/plus.json', json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+})->purpose('sort plus.json by name');
+
+
+/**
+ * Local testing commands
+ */
 // vendor/bin/testbench full
 Artisan::command('full', function () {
     dump(FullFeed::first('https://laravel.com/blog/how-we-built-laravel-wrapped'));
